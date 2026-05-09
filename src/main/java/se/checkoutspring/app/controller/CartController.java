5a0b19c1a8bd73de7d7b8ca71fd33d2ef039012c
Anton Henriksson <henriksson.anton@gmail.com>
2026-05-09 11:05:45 +0200

    changed auth to be more token safe

--- src/main/java/se/checkoutspring/app/controller/CartController.java
+++ src/main/java/se/checkoutspring/app/controller/CartController.java
@@ -2,6 +2,7 @@ package se.checkoutspring.app.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import se.checkoutspring.app.dto.CartResponse;
 import se.checkoutspring.app.dto.ItemRequest;
@@ -23,7 +24,9 @@ public class CartController {
     }
 
     @PostMapping("/{email}")
-    public ResponseEntity<CartResponse> addToCart(@PathVariable String email, @RequestBody ItemRequest request) {
+    public ResponseEntity<CartResponse> addToCart(Authentication authentication, @RequestBody ItemRequest request) {
+        String email = authentication.getName();
+
         CartResponse updatedCart = cartService.addToCart(email, request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(updatedCart);
@@ -31,20 +34,24 @@ public class CartController {
     }
 
     @DeleteMapping("/{email}/items/{productId}")
-    public ResponseEntity<Void> removeFromCart(@PathVariable String email, @PathVariable Long productId) {
+    public ResponseEntity<Void> removeFromCart(Authentication authentication, @PathVariable Long productId) {
+        String email = authentication.getName();
+
         cartService.removeFromCart(email, productId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{email}/items")
-    public ResponseEntity<CartResponse> getCart(@PathVariable String email) {
+    public ResponseEntity<CartResponse> getCart(Authentication authentication) {
+        String email = authentication.getName();
         CartResponse updatedCart = cartService.getCart(email);
 
         return ResponseEntity.ok().body(updatedCart);
     }
 
     @PostMapping("/{email}/checkout")
-    public ResponseEntity<String> checkout(@PathVariable String email) {
+    public ResponseEntity<String> checkout(Authentication authentication) {
+        String email = authentication.getName();
         // om userId balance >= cartCost { http200 }
         return null;
     }
